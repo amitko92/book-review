5857c841d7343bdb10f9a9c36400c345aca837ac
Amit Koren <30484152+amitko92@users.noreply.github.com>
2020-08-06 02:48:27 +0300

    update

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const morgan = require('express')
+const morgan = require('morgan')
+
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) =>{
+app.post('/register', (req, res) => {
     res.send({
-        massage: 'hello world!'
+        massage: `hello ${ req.body.email } your user have being register.`
     })
 })
+
+
 
 app.listen(process.env.PORT || 8081)
